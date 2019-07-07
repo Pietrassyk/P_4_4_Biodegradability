ce6d08163ad15e11ccd280ca8fa46faa49d02550
Tino Pietrassyk <pietrassyk@googlemail.com>
2019-07-07 19:31:15 -0400

    Reformate Docstrings Add Gridsearch for model Save bevor overwriting current best model with gridsearch results

---
+++ functions.py
@@ -0,0 +1,111 @@
+import itertools
+from sklearn.metrics import confusion_matrix
+import sklearn.metrics as metrics
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def evaluate_classifier(y_test, y_pred, classes = ['Not Degradable', 'Degradable'], normalize=False):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    cm = confusion_matrix(y_test,y_pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion Matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title('Confusion matrix')
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.show()
+    
+    acc = metrics.accuracy_score(y_test, y_pred)
+    prec = metrics.precision_score(y_test, y_pred)
+    sens = metrics.recall_score(y_test, y_pred)
+    f_1 = 2*(prec*sens)/(prec+sens) 
+    print('Our Accuracy is:', acc)
+    print('Our Precision is:', prec)
+    print('Our Sensitivity is:', sens )
+    print('Our F-Score is:', f_1)
+    
+    result_dict = {"accuracy": acc,
+                  "precission": prec,
+                  "sensitivity": sens,
+                  "f_1" : f_1,
+                  "confusion_matrix" : confusion_matrix(y_test,y_pred)}
+    #results_dict_list.append(result_dict)
+    return result_dict
+
+def plot_coefs(X_train,model, return_nulls = True, legend = True):
+    """Function for Plotting a Bar Chart of a Linear models Coefficients
+    
+    Parameters :
+    ------------
+    X_train : Pandas DataFrame 
+        Training Features
+    model : sklearn ModelObject 
+        Linear or Logistic Regression model with .coef_ attribute
+    return_null: bool 
+        whether or not to return Features that have been pushed a coefficent value of zero
+    legend: bool
+        show legend for each bar
+    
+    Returns :
+    -----------
+    return_null: list
+        Features with coefficient of zero
+        
+    plot_coef: Pandas DataFrame 
+        DataFrame with Feature Name and coefficient"""
+
+    plt.style.use("seaborn")
+    fig = plt.figure(figsize = (16,15))
+    plot_coef = pd.DataFrame(model.coef_[0].T, index=X_train.columns, columns = ["coeff"]).sort_values("coeff")
+    plot_coef.plot(kind = "bar")
+    if legend:
+        plt.legend()
+    plt.show()
+    if return_nulls:
+        return plot_coef.loc[plot_coef.coeff ==0].index
+    return plot_coef
+
+def compare_models(results_dict_list, names_list = None):
+    """Compares different models and pliots the mectrics values.
+
+    Attributes
+    ----------
+    results_dict_list : list of dictionaries
+        List of multiple scoring metrics of a model as key value pairs in a dictionary.
+
+    names_list : list of strings 
+        List with model Names for identification, optional.
+
+    Returns:
+    ----------
+    res_df : Pandas DataFrame
+        Pandas DataFrame Object containing the Scoring metrics for each model.
+    """
+    res_df = pd.DataFrame(columns = results_dict_list[0].keys())
+    for model in results_dict_list:
+        res_df.loc[len(res_df)] = list(model.values())
+    if names_list:
+        res_df.index = names_list
+    return res_df
